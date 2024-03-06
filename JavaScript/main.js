e97187e62aef774e7c0f41293676380bce266dd0
shakhzod <s_abdullaev@kbtu.kz>
2024-03-06 21:56:17 +0500

    Commited

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+//var --> for functions
+//let --> for blocks
+//const --> blocks 
+
+
+function sayHello(){
+    for(let i = 0; i < 5; i++){
+        console.log(i)
+    }
+    console.log(i)
+    console.log(i)
+}
+
+sayHello();
+sayHello();
